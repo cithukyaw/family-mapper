950c7ca0d87ed09690c6732c862b6de4cae3b623
Sithu <cithukyaw@gmail.com>
2017-12-31 17:21:33 +0630

    Adding marker for user current location and info window to the map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -42,4 +42,30 @@ export class HomePage {
 
   }
 
+  addMarker() {
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = '<h4>My current location!</h4>';
+
+    this.addInfoWindow(marker, content);
+
+  }
+
+   addInfoWindow(marker, content) {
+
+     let infoWindow = new google.maps.InfoWindow({
+       content: content
+     });
+
+     google.maps.event.addListener(marker, 'click', () => {
+       infoWindow.open(this.map, marker);
+     });
+
+   }
+
 }
